59bed70e496504e3183113b3053e1763ae6c8cc6
maximcuynat <maxim.cuynat@viacesi.fr>
2025-12-10 22:19:56 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,26 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        
+        // Créer un cinéma
+        Cinema cPathe = new Cinema("Pathé plan de campagne");
+
+        // Créer une salle dans le cinéma
+        Room room1 = new Room("Salle 1");
+
+        // Ajouer une séance
+        Seance seance1 = new Seance("10/12/2025", "18h15", "FNAF 2", room1);
+        room1.addSeance(seance1);
+
+        Person pMaxim = new Person("CUYNAT", "Maxim");
+        Reservation rMaxim = new Reservation(pMaxim);
+
+        seance1.reserveSeats(new int[]{2, 0}, SeatType.NORMAL);
+
+        seance1.addReservation(rMaxim);
+
+        seance1.displaySeatMap();
+
+        // Ajouter la Salle au cinéma
+        cPathe.addRoom(room1);
     }
 }
