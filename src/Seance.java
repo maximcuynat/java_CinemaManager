4582b9e83077a65b302de997faa7d93215d403ba
maximcuynat <maxim.cuynat@viacesi.fr>
2025-12-10 18:47:13 +0100

    Add displaySeatMap in Seance.java

--- src/Seance.java
+++ src/Seance.java
@@ -108,6 +108,41 @@ public class Seance
         return clone;
     }
 
+    /**
+     * Afficher la seatMap avec légende
+     */
+    public void displaySeatMap() {
+        System.out.println("\n=== Séance: " + this.movie + " - " + this.date + " " + this.time + " ===");
+        System.out.println("Salle: " + this.room.getName());
+        System.out.println();
+
+        // En-tête colonnes
+        System.out.print("    ");
+        for (int col = 0; col < seatMap[0].length; col++) {
+            System.out.print(col + " ");
+        }
+        System.out.println();
+
+        // Lignes avec numéros
+        for (int row = 0; row < seatMap.length; row++) {
+            System.out.print(row + " - ");
+            for (int col = 0; col < seatMap[row].length; col++) {
+                System.out.print(seatMap[row][col] + " ");
+            }
+            System.out.println();
+        }
+
+        // Légende
+        System.out.println("\nLégende:");
+        System.out.println("  0 = Libre");
+        System.out.println("  O = Occupé");
+        System.out.println("  X = Allée/Inexistant");
+        System.out.println("  P = PMR (libre)");
+        System.out.println("  D = Place double (libre)");
+        System.out.println("  x = Continuation place double");
+        System.out.println("\nRéservations actives: " + this.reservations.size());
+    }
+
     // ------------------------------------------- Seat
     /**
      * Système de réservation
